c89dd89b9ad8699b90495d89d12a3a4115df6dc2
Gabriela Cruz <gabrielasnts30@gmail.com>
2023-10-05 22:50:48 -0300

    first commit

---
+++ app/components/Header/header.tsx
@@ -0,0 +1,11 @@
+import './header.scss';
+
+export default function Header() {
+    return (
+        <div className="top-page-header">
+            <h1>Gabriela Cruz</h1>
+            <h2>Desenvolvedora de Software</h2>
+            <p>Eu projeto, escrevo e desenvolvo soluções utilizando minhas linguagens de programação favoritas ;)</p>
+        </div>
+    )
+}
